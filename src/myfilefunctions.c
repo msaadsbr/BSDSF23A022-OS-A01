937a442dd31c209609194a4642e958f7c4e1a25b
Muhammad Saad Sabir <saadsbr789@gmail.com>
2025-09-23 09:31:58 +0500

    Program Files Work Properly

--- src/myfilefunctions.c
+++ src/myfilefunctions.c
@@ -2,44 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int wordCount(FILE* file, int* lines, int* words, int* chars) {
     if (!file) return -1;
-    *lines = *words = *chars = 0;
 
-    char c, prev = ' ';
+    *lines = *words = *chars = 0;
+    int c, in_word = 0;
+
     while ((c = fgetc(file)) != EOF) {
         (*chars)++;
         if (c == '\n') (*lines)++;
-        if ((c == ' ' || c == '\n' || c == '\t') &&
-            !(prev == ' ' || prev == '\n' || prev == '\t')) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            in_word = 0;
+        } else if (!in_word) {
+            in_word = 1;
             (*words)++;
         }
-        prev = c;
     }
-    rewind(file); // reset file pointer for reuse
+
+    rewind(file); 
     return 0;
 }
 
-int mygrep(FILE* fp, const char* search_str, char*** matches) {
-    if (!fp || !search_str) return -1;
 
-    size_t size = 10, count = 0;
-    *matches = malloc(size * sizeof(char*));
+int mygrep(FILE* fp, const char* search_str, char*** matches) {
+    if (!fp || !search_str || !matches) return -1;
+
+    size_t linecap = 0;
+    char* line = NULL;
+    int count = 0;
+    int capacity = 10;
+
+    *matches = malloc(capacity * sizeof(char*));
     if (!*matches) return -1;
 
-    char buffer[256];
-    while (fgets(buffer, sizeof(buffer), fp)) {
-        if (strstr(buffer, search_str)) {
-            if (count >= size) {
-                size *= 2;
-                *matches = realloc(*matches, size * sizeof(char*));
+    while (getline(&line, &linecap, fp) != -1) {
+        if (strstr(line, search_str)) {
+            if (count >= capacity) {
+                capacity *= 2;
+                *matches = realloc(*matches, capacity * sizeof(char*));
                 if (!*matches) return -1;
             }
-            (*matches)[count] = strdup(buffer);
+            (*matches)[count] = strdup(line);
             count++;
         }
     }
+
+    free(line);
     rewind(fp);
-    return (int)count;
+    return count;
 }
 
--- src/mystrfunctions.c
+++ src/mystrfunctions.c
@@ -1,41 +1,46 @@
 #include "../include/mystrfunctions.h"
-#include <string.h>
 
 int mystrlen(const char* s) {
     int len = 0;
-    while (s[len] != '\0') {
+    while (s && s[len] != '\0') {
         len++;
     }
     return len;
 }
 
+
 int mystrcpy(char* dest, const char* src) {
+    if (!dest || !src) return -1;
     int i = 0;
     while (src[i] != '\0') {
         dest[i] = src[i];
         i++;
     }
     dest[i] = '\0';
-    return 0;
+    return i;
 }
 
+
 int mystrncpy(char* dest, const char* src, int n) {
+    if (!dest || !src) return -1;
     int i = 0;
     while (i < n && src[i] != '\0') {
         dest[i] = src[i];
         i++;
     }
     dest[i] = '\0';
-    return 0;
+    return i;
 }
 
+
 int mystrcat(char* dest, const char* src) {
+    if (!dest || !src) return -1;
     int i = 0, j = 0;
     while (dest[i] != '\0') i++;
     while (src[j] != '\0') {
         dest[i++] = src[j++];
     }
     dest[i] = '\0';
-    return 0;
+    return i;
 }
 
